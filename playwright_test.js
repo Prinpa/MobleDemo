82067a08e729f36a11594559b5798d3e8f74156c
Pat Dowd <pa77y04@gmail.com>
2025-08-02 16:07:26 +1000

    banner image grabbing

--- playwright_test.js
+++ playwright_test.js
@@ -1,10 +1,33 @@
+import { type } from "os";
+import { chromium } from "playwright";
+
 (async () => {
   const url = process.argv[2]; // Get URL from command line argument
   if (!url) {
     console.error('Usage: node playwright_test.js <URL>');
     process.exit(1);
   }
-  console.log("Here")
+
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+  // Wait until at least one image with 'banner' is present
+  await page.waitForSelector('img[src*="banner"]');
+
+  // get the src of only the images that contain "banner"
+  const banners = await page.$$eval('img', imgs =>
+    imgs
+      .filter(img => img.src.includes('banner'))
+      .map(img => img.src)
+  );
+
+  console.log('Banner images:', banners);
 
 
+  const h1Headers = await page.getByRole("heading", {level: 1}).allInnerTexts()
+  console.log("h1Headers:" + h1Headers)
+  
+  
+  await browser.close()
 })();
