60d43a8e4756a5dc6a0a0a49e27720948523df5d
Pat Dowd <pa77y04@gmail.com>
2025-08-03 17:28:18 +1000

    contact details

--- playwright_test.js
+++ playwright_test.js
@@ -1,4 +1,3 @@
-import { type } from "os";
 import { chromium } from "playwright";
 
 (async () => {
@@ -7,7 +6,11 @@ import { chromium } from "playwright";
     console.error('Usage: node playwright_test.js <URL>');
     process.exit(1);
   }
+  let infoHolder = {}
+  let contactInfo = {}
 
+  infoHolder.url = url
+  // start the browser
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'domcontentloaded' });
@@ -21,13 +24,25 @@ import { chromium } from "playwright";
       .filter(img => img.src.includes('banner'))
       .map(img => img.src)
   );
+  infoHolder.bannerImages = banners
 
-  console.log('Banner images:', banners);
+  // get every ehader of level 1 (h1)
+  let h1Headers = await page.getByRole("heading", {level: 1}).allInnerTexts()
+  h1Headers = h1Headers.toString().split(",")
+  infoHolder.h1Headers = h1Headers
 
+  // contact information
+  const address = await page.$eval('address', address => address.innerText);
+  contactInfo.address = address
 
-  const h1Headers = await page.getByRole("heading", {level: 1}).allInnerTexts()
-  console.log("h1Headers:" + h1Headers)
-  
-  
+  const phoneNumber = await page.getByLabel("phone number").allInnerTexts()
+  contactInfo.phone = phoneNumber[0].trim()
+
+  // tack on the contactInformation object
+  infoHolder.contactInfo = contactInfo
+
+  // log the final result
+  console.log(infoHolder)
+
   await browser.close()
 })();
